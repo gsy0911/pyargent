8305ce7078314c3d6402597c47d51d05a019cbf4
yoshiki <yoshiki0911@gmail.com>
2021-01-18 22:59:04 +0900

    #1 add OneFile class

---
+++ pyargent/credit_card/one_file.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from dataclasses import field
+from typing import List
+
+from .one_row import OneRow
+
+
+@dataclass(init=True, order=True)
+class OneFile:
+    raw_text_list: List[str] = field(default_factory=list, repr=False, compare=False)
+    one_row_list: List[OneRow] = field(default_factory=list, compare=False)
+
+    @staticmethod
+    def from_file_path(file_path: str, encoding="cp932"):
+        with open(file_path, mode="r", encoding=encoding) as f:
+            text_list = [s for s in f.readlines()]
+
+        one_row_list = [OneRow.from_text(t) for t in text_list]
+        one_row_list = [c for c in one_row_list if c is not None]
+        return OneFile(raw_text_list=text_list, one_row_list=one_row_list)
+
+    @staticmethod
+    def from_file_path_list(file_path_list: List[str], encoding="cp932"):
+        whole_raw_text_list = []
+        whole_one_row_list = []
+
+        for file_path in file_path_list:
+            one_file = OneFile.from_file_path(file_path, encoding)
+            whole_raw_text_list.extend(one_file.raw_text_list)
+            whole_one_row_list.extend(one_file.one_row_list)
+        return OneFile(raw_text_list=whole_raw_text_list, one_row_list=whole_one_row_list)
