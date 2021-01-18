1822fb2ee6d470cdb2341ca183b4c445cccba9b5
yoshiki <yoshiki0911@gmail.com>
2021-01-18 23:26:17 +0900

    #1 fix: change dtypes

--- pyargent/credit_card/one_file.py
+++ pyargent/credit_card/one_file.py
@@ -31,5 +31,13 @@ class OneFile:
             whole_one_row_list.extend(one_file.one_row_list)
         return OneFile(raw_text_list=whole_raw_text_list, one_row_list=whole_one_row_list)
 
-    def to_df(self):
-        return pd.DataFrame([asdict(c) for c in self.one_row_list])
+    def to_df(self) -> pd.DataFrame:
+        df = pd.DataFrame([asdict(c) for c in self.one_row_list])
+        df = df.replace({"total_billing": "", "count": "", "num": "", "actual_billing": ""}, "0")
+        df = df.astype({
+            "total_billing": "int",
+            "count": "int",
+            "num": "int",
+            "actual_billing": "int"
+        })
+        return df
